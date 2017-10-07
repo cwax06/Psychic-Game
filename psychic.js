7304642b21d4363998cf87014010f0236ee77cf6
Andrew Jones <andrew@Andrews-MacBook-Pro.local>
2017-10-07 11:13:08 -0700

    random letter works

--- psychic.js
+++ psychic.js
@@ -1,33 +1,41 @@
 
-
 // Array of possible answers
 
 var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
 
+// Computer randomly selects a letter (stuck trying to select a string rather than number)
+
+var randomLetter = letters[Math.floor(Math.random() * letters.length)];
+
 var userText = document.getElementById("user-text");
 
-// Computer randomly selects a letter
-
-$(document).ready(function () {
-
-    letters[Math.floor(Math.random() * letters.length)];
-    console.log("checking")
-
-
-    // Display the number of Wins
+// Display the string of randomNumber
 
 
 
-    // Display the number of Losses
+console.log(randomLetter)
+
+function inArray(userText, letters) {
+    var length = letters.length;
+    for (var i = 0; i < length; i++) {
+        if (letters[i] == userText)
+            return true;
+    }
+    return false;
+}
+
+inArray()
+// Display the number of Wins
 
 
-    // User Enters
+
+// Display the number of Losses
 
 
-};
+// User Enters
 
 
-});
+
 
 document.onkeyup = function (event) {
     userText.textContent = event.key;
