000db5ea6850d4a319935831de8e3fe19f83d6ec
Andrew Jones <andrew@Andrews-MacBook-Pro.local>
2017-10-07 20:32:28 -0700

    working start

--- psychic.js
+++ psychic.js
@@ -1,44 +1,37 @@
 
 // Array of possible answers
 
-var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
+var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-// Computer randomly selects a letter (stuck trying to select a string rather than number)
+// Computer randomly selects a letter
 
 var randomLetter = letters[Math.floor(Math.random() * letters.length)];
 
 var userText = document.getElementById("user-text");
 
+
 // Display the string of randomNumber
-
-
 
 console.log(randomLetter)
 
-function inArray(userText, letters) {
-    var length = letters.length;
-    for (var i = 0; i < length; i++) {
-        if (letters[i] == userText)
-            return true;
-    }
-    return false;
-}
+// compare randomly generated letter with user's input
 
-inArray()
+
+// Start function
+
+
+
+
+// Listen for user input
+document.onkeyup = function (event) {
+    userText.innerHTML = event.key
+};
+
+
+
 // Display the number of Wins
-
-
 
 // Display the number of Losses
 
 
 // User Enters
-
-
-
-
-document.onkeyup = function (event) {
-    userText.textContent = event.key;
-}
-
-console.log("hello world")
